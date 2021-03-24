90c297379876f38d4dab98ea72d9502607f95ba1
mgrybel <monika.grybel@hotmail.com>
2021-03-24 20:36:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
 import Navigator from './navigation/Navigator';
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import { colors } from './styles/styles';
 
 class App extends React.Component {
-  render () {
-    return (
-      <Navigator />
-    );
-  }
+    render () {
+        return (
+            <Navigator />
+        );
+    }
 }
 
 export default App;
